8bd30d94b6b019ab05ac9b187b334166fd381691
OfriG <ofri900@gmail.com>
2024-07-28 12:29:55 +0300

    author_server side

---
+++ serverSide/serverSide/BL/Author.cs
@@ -0,0 +1,19 @@
+﻿namespace serverSide.BL
+
+public class Author
+{
+
+    string name = string.empty;
+     int id;
+
+    public Author(int id, string name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+    static public List<Author> AuthorList;
+    public int Id { get => id; set => id = value; }
+    public string Name { get => name; set => name = value; }
+
+    public static Author addNewAuthor(int id, string name) { }
+}
---
+++ serverSide/serverSide/DAL/DBservicesAuthor.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+using Task1.BL;
+using System.Data.Common;
+
+/// <summary>
+/// DBServices is a class created by me to provides some DataBase Services
+/// </summary>
+public class DBservicesAuthor
+{
+
+    public DBservicesAuthor() { }
+
+    //--------------------------------------------------------------------------------------------------
+    // This method creates a connection to the database according to the connectionString name in the web.config 
+    //--------------------------------------------------------------------------------------------------
+    public SqlConnection connect(String conString)
+    {
+
+        // read the connection string from the configuration file
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json").Build();
+        string cStr = configuration.GetConnectionString("myProjDB");
+        SqlConnection con = new SqlConnection(cStr);
+        con.Open();
+        return con;
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    // This method adds a new author to the authors table 
+    //--------------------------------------------------------------------------------------------------
+    public int AddNewAuthor(Author author)
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+
+        try
+        {
+            con = connect("myProjDB"); // create the connection
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        cmd = CreateCommandWithStoredProcedureAddNewUser("addNewAuthor", con, author);             // create the command
+
+        try
+        {
+            int numEffected = cmd.ExecuteNonQuery(); // execute the command
+            return numEffected;
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        finally
+        {
+            if (con != null)
+            {
+                // close the db connection
+                con.Close();
+            }
+        }
+
+    }
+
+
+    //---------------------------------------------------------------------------------
+    // Create the SqlCommand using a stored procedure to add new author
+    //---------------------------------------------------------------------------------
+
+    private SqlCommand CreateCommandWithStoredProcedureAddNewAuthor(String spName, SqlConnection con, Author author)
+    {
+
+        SqlCommand cmd = new SqlCommand(); // create the command object
+
+        cmd.Connection = con;              // assign the connection to the command object
+
+        cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+        cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+        cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be text
+
+        cmd.Parameters.AddWithValue("@name", author.name);
+
+        cmd.Parameters.AddWithValue("@id", author.id);
+
+
+        return cmd;
+    }
+
