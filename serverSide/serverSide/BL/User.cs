21f523de710c1b6e64669e9ec6448c2de06dfe18
AdiDagan1 <adidagan2271@gmail.com>
2024-07-28 10:18:37 +0300

    user_serverSide

---
+++ serverSide/serverSide/BL/User.cs
@@ -0,0 +1,60 @@
+using System.Reflection.Metadata.Ecma335;
+
+namespace serverSide.BL
+{
+    public class User
+    {
+        int id;
+        string userName = string.Empty;
+        string eMail = string.Empty;
+        string password = string.Empty;
+        bool isAdmin = false;
+        bool isActive = true;
+        bool isLogIn = false; // Because there is only one user logged in every time --> an indecator for us to know
+        public User() { }
+
+
+        public User(string userName, string eMail, string password)
+        {
+            this.userName = userName;
+            this.eMail = eMail;
+            this.password = password;
+            
+        }
+        public int Id { get => id; set => id = value; }
+        public string userName { get => userName; set => userName = value; }
+        public string EMail { get => eMail; set => eMail = value; }
+        public string Password { get => password; set => password = value; }
+        public bool IsAdmin { get => isAdmin; set => isAdmin = value; }
+        public bool IsActive { get => isActive; set => isActive = value; }
+        public bool IsLogIn { get => isLogIn; set => isLogIn = value; }
+
+        static public List<User> ReadUsers() { return usersList; }
+        static public List<Book> allBooks() { return myBooks; }
+
+        
+
+
+        // Add new book to the user's books list
+        public static bool addNewBook(Book book)
+        {
+            // Check if the book is already in my list --> if it is return false
+
+           
+
+        }
+
+
+
+        // Delete book from my book list --> by book id
+        public static bool DeleteBookById(int id)
+        {
+           
+        }
+
+        
+
+
+       
+    }
+}
---
+++ serverSide/serverSide/DAL/DBserviceUsers.cs
@@ -0,0 +1,299 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+using Task1.BL;
+using System.Data.Common;
+
+/// <summary>
+/// DBServices is a class created by me to provides some DataBase Services
+/// </summary>
+public class DBservicesUsers
+{
+
+    public DBservicesUsers() { }
+
+    //--------------------------------------------------------------------------------------------------
+    // This method creates a connection to the database according to the connectionString name in the web.config 
+    //--------------------------------------------------------------------------------------------------
+    public SqlConnection connect(String conString)
+    {
+
+        // read the connection string from the configuration file
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json").Build();
+        string cStr = configuration.GetConnectionString("myProjDB");
+        SqlConnection con = new SqlConnection(cStr);
+        con.Open();
+        return con;
+    }
+
+   
+    //--------------------------------------------------------------------------------------------------
+    // This method adds a new user to the users table 
+    //--------------------------------------------------------------------------------------------------
+    public int AddNewUser(User user )
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+
+        try
+        {
+            con = connect("myProjDB"); // create the connection
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        cmd = CreateCommandWithStoredProcedureAddNewUser("AddNewUserBook", con, user);             // create the command
+
+        try
+        {
+            int numEffected = cmd.ExecuteNonQuery(); // execute the command
+            return numEffected;
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        finally
+        {
+            if (con != null)
+            {
+                // close the db connection
+                con.Close();
+            }
+        }
+
+    }
+
+
+    //---------------------------------------------------------------------------------
+    // Create the SqlCommand using a stored procedure to add new user
+    //---------------------------------------------------------------------------------
+
+    private SqlCommand CreateCommandWithStoredProcedureAddNewUser(String spName, SqlConnection con, User user)
+    {
+
+        SqlCommand cmd = new SqlCommand(); // create the command object
+
+        cmd.Connection = con;              // assign the connection to the command object
+
+        cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+        cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+        cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be text
+
+        cmd.Parameters.AddWithValue("@name", user.userNameName);
+
+        cmd.Parameters.AddWithValue("@email", user.EMail);
+
+        cmd.Parameters.AddWithValue("@password", user.Password);
+
+        return cmd;
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    // login user
+    //--------------------------------------------------------------------------------------------------
+    public int logInUser(User user)
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+
+        try
+        {
+            con = connect("myProjDB"); // create the connection
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        cmd = CreateCommandWithStoredProcedurelogInUser("Login", con, user);             // create the command
+
+        try
+        {
+            int numEffected = cmd.ExecuteNonQuery(); // execute the command
+            return numEffected;
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        finally
+        {
+            if (con != null)
+            {
+                // close the db connection
+                con.Close();
+            }
+        }
+
+    }
+
+
+    //---------------------------------------------------------------------------------
+    // Create the SqlCommand using a stored procedure to login user
+    //---------------------------------------------------------------------------------
+
+    private SqlCommand CreateCommandWithStoredProcedurelogInUser(String spName, SqlConnection con, IUser user)
+    {
+
+        SqlCommand cmd = new SqlCommand(); // create the command object
+
+        cmd.Connection = con;              // assign the connection to the command object
+
+        cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+        cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+        cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be text
+
+        cmd.Parameters.AddWithValue("@email", user.EMail);
+
+        cmd.Parameters.AddWithValue("@password", user.Password);
+
+        return cmd;
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    // This method return the login user
+    //--------------------------------------------------------------------------------------------------
+    public IUser GetLoginUser()
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+
+        try
+        {
+            con = connect("myProjDB"); // create the connection
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        cmd = CreateCommandWithStoredProcedureGetLoginUser("GetLoginUser", con);             // create the command
+
+        IUser user = new IUser();
+        try
+        {
+            SqlDataReader dataReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+            if (dataReader.HasRows)
+            {
+                dataReader.Read();
+                user.Name = Convert.ToString(dataReader["name"]);
+                user.IsAdmin = (Convert.ToInt32(dataReader["isAdmin"])== 1);
+                user.Id = Convert.ToInt32(dataReader["userId"]);
+
+                return user;
+            }
+            else return null;
+
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        finally
+        {
+            if (con != null)
+            {
+                // close the db connection
+                con.Close();
+            }
+        }
+
+    }
+
+
+    //---------------------------------------------------------------------------------
+    // Create the SqlCommand using a stored procedure to get login user
+    //---------------------------------------------------------------------------------
+
+    private SqlCommand CreateCommandWithStoredProcedureGetLoginUser(String spName, SqlConnection con)
+    {
+
+        SqlCommand cmd = new SqlCommand(); // create the command object
+
+        cmd.Connection = con;              // assign the connection to the command object
+
+        cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+        cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+        cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be text
+
+        return cmd;
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    // This method logout the log in user
+    //--------------------------------------------------------------------------------------------------
+    public void logOut() 
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+
+        try
+        {
+            con = connect("myProjDB"); // create the connection
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        cmd = CreateCommandWithStoredProcedureLogOut("Logout", con);             // create the command
+
+
+        try
+        {
+            SqlDataReader dataReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+          
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        finally
+        {
+            if (con != null)
+            {
+                // close the db connection
+                con.Close();
+            }
+        }
+
+    }
+
+
+    
+
+}
