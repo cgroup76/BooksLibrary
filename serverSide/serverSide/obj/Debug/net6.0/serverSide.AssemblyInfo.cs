21726333689136052a90848dac0c7c9817d23453
AdiDagan1 <adidagan2271@gmail.com>
2024-08-02 18:24:14 +0300

    send request

--- serverSide/serverSide/obj/Debug/net6.0/serverSide.AssemblyInfo.cs
+++ serverSide/serverSide/obj/Debug/net6.0/serverSide.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("serverSide")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0391758dad7d156ad0f55833d0125682d3406f0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("serverSide")]
 [assembly: System.Reflection.AssemblyTitleAttribute("serverSide")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
